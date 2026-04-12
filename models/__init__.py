35e00b8c91c358dbdd9bc71827edc7f645d91bb0
Eryx G.F <eryxgrammaticas@gmail.com>
2026-04-12 21:22:51 +0200

    feat: add __init__.py (module, "exports" all other files)

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .account import BankAccount
+from .savings_account import SavingsAccount
+from .current_account import CurrentAccount
+from .bank import Bank
+from .transaction import Transaction
